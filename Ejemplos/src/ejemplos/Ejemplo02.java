ca6b157c6f228b7fb1248c1476f7a21b9f0b5d1a
alex <alexis.rj2110@gmail.com>
2023-01-18 10:27:18 -0500

    Subida3

--- Ejemplos/src/ejemplos/Ejemplo02.java
+++ Ejemplos/src/ejemplos/Ejemplo02.java
@@ -14,41 +14,44 @@ import java.util.Scanner;
  * @author reroes
  */
 public class Ejemplo02 {
-    
+
     public static void main(String[] args) {
         // TODO code application logic here
-        int limite;
+
         Scanner entrada = new Scanner(System.in);
+
+        int limite;
         System.out.println("Ingrese el limite de "
                 + "generacion de los numeros aleatorios, "
                 + "el limite debe ser positivo");
         limite = entrada.nextInt();
-        if (limite>0) {
-           int valorA = obtenerNumero(limite); // 3
-        int valorB = obtenerNumero(limite); // 2
-        int suma = obtnerSuma(valorA, valorB);
-        System.out.printf("La suma de %d + %d es igual a: %d\n", 
-                valorA,
-                valorB,
-                suma); 
-        }else{
+
+        if (limite > 0) {
+            int valorA = obtenerNumero(limite); // 3
+            int valorB = obtenerNumero(limite); // 2
+            int suma = obtnerSuma(valorA, valorB);
+            System.out.printf("La suma de %d + %d es igual a: %d\n",
+                    valorA,
+                    valorB,
+                    suma);
+        } else {
             System.out.println("El numero debe ser positivo");
         }
-        
+
     }
-    
+
     public static int obtenerNumero(int limite) {
         // objeto generador de números aleatorios
         SecureRandom numerosAleatorios = new SecureRandom();
-        
+
         // Returns a pseudorandom, uniformly distributed int value 
         // between 0 (inclusive) and the specified value (exclusive)
         int valorAleatorio = numerosAleatorios.nextInt(limite);
         return valorAleatorio;
     }
-    
-    public static int obtnerSuma(int a, int b){
+
+    public static int obtnerSuma(int a, int b) {
         return a + b;
     }
-    
+
 }
