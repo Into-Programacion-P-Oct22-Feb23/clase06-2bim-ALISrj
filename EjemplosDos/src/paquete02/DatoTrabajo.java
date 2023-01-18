3f6d968e97630f1374c188cc29fa7453101867a1
alex <alexis.rj2110@gmail.com>
2023-01-18 10:43:28 -0500

    Subida3

--- EjemplosDos/src/paquete02/DatoTrabajo.java
+++ EjemplosDos/src/paquete02/DatoTrabajo.java
@@ -5,12 +5,31 @@
  */
 package paquete02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class DatoTrabajo {
+
     // método que permita preguntar al usuario el nombre de la empresa
-    
+    public static String empresa() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese nombre de la empresa:");
+        String empresa = entrada.nextLine();
+
+        return empresa;
+
+    }
+
+    public static String direccion() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese la direccion de la empresa:");
+        String empresa = entrada.nextLine();
+
+        return empresa;
+    }
+
     // método que permita preguntar al usuario la dirección de la empresa
 }
